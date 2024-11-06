df59df3641f747f382367a2617d2bb150cb5b829
John Yu <john@mac.lan>
2024-11-06 16:11:25 -0500

    add install_pygraphviz.sh

---
+++ install_pygraphviz.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+pip install --config-settings="--global-option=build_ext" \
+            --config-settings="--global-option=-I$(brew --prefix graphviz)/include/" \
+            --config-settings="--global-option=-L$(brew --prefix graphviz)/lib/" \
+            pygraphviz
