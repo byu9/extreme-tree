95d9c7b5665459c6dcffd780a9919fcdc8411ab4
John Yu <byu9@ncsu.edu>
2025-03-03 11:01:50 -0500

    modify compile_pjm.py

--- datasets/pjm/compile_pjm.py
+++ datasets/pjm/compile_pjm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import norm
 
 
 def to_datetime(arr):
@@ -60,43 +61,63 @@ def compile_fragments(filenames, read_func):
     return dataset
 
 
-def compile_datasets():
+def compile_generation():
     generation_files = [
         f'raw/day_gen_capacity-{year}.csv'
         for year in [2020, 2021, 2022, 2023, 2024]
     ]
+    generation = compile_fragments(generation_files, read_func=read_generation)
+    generation = generation.resample('1h').mean()
+    generation.index = generation.index.tz_localize(None)
+    return generation
 
-    load_files = [
-        f'raw/hrl_load_prelim-{year}.csv'
-        for year in [2020, 2021, 2022, 2023, 2024]
-    ]
 
+def compile_temperature():
     temperature_files = [
         f'raw/da_tempset-{year}.csv'
+        for year in [2020, 2021, 2022, 2023, 2024]
+    ]
+    temperature = compile_fragments(temperature_files, read_func=read_temperature)
+    temperature = temperature.drop(columns=['Zone', 'Kind']).groupby(['Time']).mean()
+    temperature = temperature.resample('1h').ffill()
+    return temperature
+
+
+def compile_load():
+    load_files = [
+        f'raw/hrl_load_estimated-{year}.csv'
         for year in [2020, 2021, 2022, 2023, 2024]
     ]
 
     load = compile_fragments(load_files, read_func=read_load)
     load = load.drop(columns='Zone').groupby('Time').sum()
+    load = load.resample('5min').ffill()
 
-    temperature = compile_fragments(temperature_files, read_func=read_temperature)
-    temperature = temperature.drop(columns=['Zone', 'Kind']).groupby(['Time']).mean()
+    noise = norm(loc=0, scale=load['MW'] / 150e3 * 5e3).rvs(len(load))
+    load['MW'] += noise
 
-    generation = compile_fragments(generation_files, read_func=read_generation)
-    generation = generation.resample('1d').mean()
-    generation.index = generation.index.tz_localize(None)
-    generation.to_csv('generation.csv')
+    return load
 
-    daily_max = load.resample('1d').max()
-    shifted_max = daily_max.shift([1, 2, 7])
 
-    forecasting = daily_max.join([shifted_max, temperature], sort=True)
-    forecasting.dropna(axis='index', inplace=True)
+def compile_datasets():
+    generation = compile_generation()
+    temperature = compile_temperature()
+    load = compile_load()
+
+    generation.to_csv('generation.csv', float_format='%.4f')
+    load.to_csv('load.csv', float_format='%.4f')
+
+    load_hourly_max = load.resample('1h').max()
+    shifted_max = load_hourly_max.shift([1, 2, 3, 7, 14])
+
+    forecasting = load_hourly_max.join([shifted_max, temperature], sort=True).ffill()
+    forecasting.dropna(inplace=True, axis='index')
     forecasting['Day'] = forecasting.index.day
     forecasting['DoW'] = forecasting.index.dayofweek
     forecasting['Month'] = forecasting.index.month
+    forecasting['Hour'] = forecasting.index.hour
     forecasting.index = forecasting.index.tz_localize(None)
-    forecasting.to_csv('forecasting.csv')
+    forecasting.to_csv('forecasting.csv', float_format='%.4f')
 
 
 compile_datasets()
