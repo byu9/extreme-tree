0658924448f85e98ee75fb961ef4667d9ce09443
John Yu <byu9@ncsu.edu>
2025-03-05 14:53:37 -0500

    modify multiple files
    modify extreme_tree.py
    remove partition.py
    
    use log score as splitting criteria

--- extreme_tree/extreme_tree.py
+++ extreme_tree/extreme_tree.py
@@ -1,48 +1,110 @@
+from collections import deque
+from operator import attrgetter
+
 import numpy as np
 from tqdm.auto import tqdm
 
 from .binary_trees import BinaryTree
-from .equal_distributions import anderson_darling
-from .equal_distributions import kolmogorov_smirnov
 from .genextreme import GenExtreme
-from .partition import Partition
 from .validation import validate_feature
 from .validation import validate_feature_target
 
-_supported_distributions = {
-    'gev': GenExtreme,
-}
 
-_supported_statistic = {
-    'ks': kolmogorov_smirnov,
-    'anderson': anderson_darling,
-}
+class _Partition:
+    __slots__ = (
+        'feature',
+        'target',
+    )
+
+    def __init__(self, feature, target):
+        self.feature = feature
+        self.target = target
+
+    def split(self, feature_id, threshold):
+        split_mask = self.feature[feature_id] <= threshold
+        left_part = _Partition(self.feature[:, split_mask], self.target[split_mask])
+        right_part = _Partition(self.feature[:, ~split_mask], self.target[~split_mask])
+        return left_part, right_part
+
+    def splitting_rules(self, min_partition_size):
+        n_features, n_samples = self.feature.shape
+
+        for feature_id in tqdm(range(n_features), desc='Feature', leave=False):
+            sort_indices = self.feature[feature_id].argsort()
+            sort_feature = self.feature[:, sort_indices]
+            sort_target = self.target[sort_indices]
+
+            unique_values = np.unique(sort_feature[feature_id])
+            midpoints = (unique_values[:-1] + unique_values[1:]) / 2
+
+            for threshold in tqdm(midpoints, desc='Threshold', leave=False):
+                split_index = np.searchsorted(sort_feature[feature_id], threshold, side='right')
+
+                if min_partition_size <= split_index <= n_samples - min_partition_size:
+                    left_target = sort_target[:split_index]
+                    right_target = sort_target[split_index:]
+
+                    yield feature_id, threshold, left_target, right_target
+
+
+class _TreeNode:
+    __slots__ = (
+        'partition',
+
+        'feature_id',
+        'threshold',
+        'impurity_drop',
+
+        'prediction',
+        'impurity',
+        'pi',
+    )
+
+    def __init__(self, partition):
+        self.partition = partition
+        self.pi = None
+
+        self.impurity_drop = -np.inf
+        self.feature_id = None
+        self.threshold = None
+        self.prediction = None
+        self.impurity = None
+
+    def find_optimal_rule(self, min_partition_size, distribution):
+        self.prediction = distribution.estimate(self.partition.target)
+        self.impurity = distribution.impurity(self.partition.target, params=self.prediction)
+        rules = self.partition.splitting_rules(min_partition_size=min_partition_size)
+
+        for feature_id, threshold, left_target, right_target in rules:
+
+            left_params = distribution.estimate(left_target)
+            right_params = distribution.estimate(right_target)
+
+            left_impurity = distribution.impurity(left_target, params=left_params)
+            right_impurity = distribution.impurity(right_target, params=right_params)
+            impurity_drop = self.impurity - left_impurity - right_impurity
+
+            if impurity_drop > self.impurity_drop:
+                self.impurity_drop = impurity_drop
+                self.feature_id = feature_id
+                self.threshold = threshold
 
 
 class ExtremeTree:
     __slots__ = (
         '_min_partition_size',
-        '_alpha',
+        '_min_impurity_drop',
         '_max_n_splits',
         '_distribution',
-        '_statistic',
         '_tree',
     )
 
-    def __init__(
-            self,
-            distribution='gev',
-            max_n_splits=40,
-            min_partition_size=3,
-            alpha=0.05,
-            statistic='anderson',
-    ):
-
+    def __init__(self, distribution=GenExtreme(), max_n_splits=20, min_partition_size=5,
+                 min_impurity_drop=0.0):
         self._min_partition_size = min_partition_size
-        self._alpha = alpha
         self._max_n_splits = max_n_splits
-        self._distribution = _supported_distributions[distribution]()
-        self._statistic = _supported_statistic[statistic]
+        self._min_impurity_drop = min_impurity_drop
+        self._distribution = distribution
         self._tree = None
 
     def _ensure_fitted(self):
@@ -50,44 +112,37 @@ class ExtremeTree:
             raise RuntimeError('Model is not fitted.')
 
     def _build_tree(self, feature, target):
-        root_node = Partition(feature, target)
-        root_node.evolve(
-            min_partition_size=self._min_partition_size,
-            statistic_func=self._statistic
-        )
-
         self._tree = BinaryTree()
+        splitting_queue = deque()
+
+        root_node = _TreeNode(_Partition(feature, target))
         self._tree.add_node(root_node)
+        splitting_queue.append(root_node)
 
         for _ in tqdm(range(self._max_n_splits), desc='Split', leave=False):
-            candidate_leaves = [
-                leaf for leaf in self._tree.leaves
-                if leaf.statistic is not None
-            ]
+            while splitting_queue:
+                node = splitting_queue.popleft()
+                node.find_optimal_rule(min_partition_size=self._min_partition_size,
+                                       distribution=self._distribution)
 
-            if not candidate_leaves:
+            leaf = max(self._tree.leaves, key=attrgetter('impurity_drop'), default=None)
+
+            if leaf is None:
                 break
 
-            statistics = np.array([leaf.statistic for leaf in candidate_leaves])
-            best_leaf = candidate_leaves[statistics.argmax()]
+            if not leaf.impurity_drop > self._min_impurity_drop:
+                break
 
-            left_child, right_child = best_leaf.split()
+            left_part, right_part = leaf.partition.split(leaf.feature_id, leaf.threshold)
 
-            left_child.evolve(
-                min_partition_size=self._min_partition_size,
-                statistic_func=self._statistic
-            )
+            left_child = _TreeNode(left_part)
+            right_child = _TreeNode(right_part)
 
-            right_child.evolve(
-                min_partition_size=self._min_partition_size,
-                statistic_func=self._statistic
-            )
+            self._tree.add_node(left_child, parent=leaf, is_left=True)
+            self._tree.add_node(right_child, parent=leaf, is_left=False)
 
-            self._tree.add_node(left_child, parent=best_leaf, is_left=True)
-            self._tree.add_node(right_child, parent=best_leaf, is_left=False)
-
-        for leaf in self._tree.leaves:
-            leaf.params = self._distribution.estimate(leaf.target)
+            splitting_queue.append(left_child)
+            splitting_queue.append(right_child)
 
     def _forward_prop(self, feature):
         n_features, n_samples = feature.shape
@@ -103,8 +158,8 @@ class ExtremeTree:
                 right_child.pi = node.pi * (feature_val > node.threshold)
 
         pi = np.stack([leaf.pi for leaf in self._tree.leaves], axis=-1)
-        params = np.stack([leaf.params for leaf in self._tree.leaves], axis=-1)
-        prediction = np.nansum(pi * params, axis=-1, keepdims=True)
+        predictions = np.stack([leaf.prediction for leaf in self._tree.leaves], axis=-1)
+        prediction = np.nansum(pi * predictions, axis=-1, keepdims=True)
 
         return prediction
 
