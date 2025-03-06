895c0492ec38d3f1754bdff8956aa1fc6b57026c
John Yu <byu9@ncsu.edu>
2025-03-05 20:57:53 -0500

    modify pjm.py
    add common functions

--- pjm.py
+++ pjm.py
@@ -1,40 +1,60 @@
 #!/usr/bin/env python3
 import pandas as pd
+from scipy.stats import genextreme
 
 from extreme_tree import ExtremeForest
 
 
-def load_dataset():
-    dataset = pd.read_csv('datasets/pjm/forecasting.csv', index_col=0)
+def read_training():
+    dataset = pd.read_csv('datasets/pjm/training.csv', index_col=0)
     dataset.index = pd.to_datetime(dataset.index, utc=True)
-    dataset = dataset.resample('1h').ffill()
-
     feature = dataset.drop(columns='MW')
-    target = dataset[['MW']]
-
-    train_mask = dataset.index < '2024'
-    test_mask = (dataset.index >= '2024')
-
-    train = (feature[train_mask], target[train_mask])
-    test = (feature[test_mask], target[test_mask])
-
-    return train, test
+    target = dataset['MW']
+    return feature, target
 
 
-def main():
-    (train_feature, train_target), (test_feature, test_target) = load_dataset()
+def read_testing():
+    dataset = pd.read_csv('datasets/pjm/testing.csv', index_col=0)
+    dataset.index = pd.to_datetime(dataset.index, utc=True)
+    feature = dataset.drop(columns='MW')
+    target = dataset['MW']
+    return feature, target
 
-    model = ExtremeForest(ensemble_size=10, resample_ratio=0.9, min_partition_size=10)
-    model.fit(train_feature, train_target)
-    mu, sigma, xi = model.predict(test_feature)
 
-    prediction = pd.DataFrame({
-        'mu': mu.ravel(),
-        'sigma': sigma.ravel(),
-        'xi': xi.ravel()
-    }, index=test_feature.index)
+def read_prediction():
+    prediction = pd.read_csv('pjm_prediction.csv', index_col=0)
+    prediction.index = pd.to_datetime(prediction.index, utc=True)
+    return prediction
 
+
+def read_prediction_extended():
+    prediction = read_prediction()
+    predict_dist = genextreme(loc=prediction['mu'], scale=prediction['sigma'], c=-prediction['xi'])
+
+    extended_data = {
+        'mean': predict_dist.mean().ravel(),
+        'lo': predict_dist.ppf(0.05).ravel(),
+        'hi': predict_dist.ppf(0.95).ravel(),
+    }
+    extended = pd.DataFrame(extended_data, index=prediction.index)
+    return extended
+
+
+def write_prediction(index, mu, sigma, xi):
+    prediction_dict = {'mu': mu.ravel(), 'sigma': sigma.ravel(), 'xi': xi.ravel()}
+    prediction = pd.DataFrame(prediction_dict, index=index)
     prediction.to_csv('pjm_prediction.csv')
 
 
-main()
+def main():
+    train_feature, train_target = read_training()
+    test_feature, test_target = read_testing()
+
+    model = ExtremeForest(ensemble_size=1, resample_ratio=1, min_partition_size=6)
+    model.fit(train_feature, train_target)
+    mu, sigma, xi = model.predict(test_feature)
+    write_prediction(test_feature.index, mu, sigma, xi)
+
+
+if __name__ == '__main__':
+    main()
