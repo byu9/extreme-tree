337702c414b997f4ca9f1090362e6e2294f1611b
John Yu <byu9@ncsu.edu>
2025-03-07 18:11:01 -0500

    predict pjm forecast residuals

--- datasets/pjm/compile_pjm.py
+++ datasets/pjm/compile_pjm.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 
 def to_datetime(arr):
-    localized = pd.to_datetime(arr, format='%m/%d/%Y %I:%M:%S %p').tz_localize('UTC')
-    converted = localized.tz_convert('US/Eastern')
+    localized = pd.to_datetime(arr, format='%m/%d/%Y %I:%M:%S %p').dt.tz_localize('UTC')
+    converted = localized.dt.tz_convert('US/Eastern')
     return converted
 
 
@@ -19,10 +19,10 @@ def read_generation(filename):
         filename,
         header=1,
         usecols=[0, 3],
-        names=['Time', 'MW'],
-        index_col=0
+        names=['Time', 'MW']
     )
-    dataset.index = to_datetime(dataset.index)
+    dataset['Time'] = to_datetime(dataset['Time'])
+    dataset.set_index('Time', inplace=True, drop=True)
     return dataset
 
 
@@ -31,10 +31,10 @@ def read_load(filename):
         filename,
         header=1,
         usecols=[0, 4, 5],
-        names=['Time', 'Zone', 'MW'],
-        index_col=0
+        names=['Time', 'Zone', 'MW']
     )
-    dataset.index = to_datetime(dataset.index)
+    dataset['Time'] = to_datetime(dataset['Time'])
+    dataset.set_index('Time', inplace=True, drop=True)
     return dataset
 
 
@@ -43,11 +43,11 @@ def read_temperature(filename):
         filename,
         header=1,
         usecols=[0, 4, 5],
-        names=['Time', 'Zone', 'Value Kind'],
-        index_col=0
+        names=['Time', 'Zone', 'Value Kind']
     )
-    dataset.index = to_datetime(dataset.index)
     dataset['Degrees F'], dataset['Kind'] = zip(*dataset.pop('Value Kind').apply(parse_temperature))
+    dataset['Time'] = to_datetime(dataset['Time'])
+    dataset.set_index('Time', inplace=True, drop=True)
     return dataset
 
 
@@ -66,8 +66,6 @@ def compile_generation():
         for year in [2020, 2021, 2022, 2023, 2024]
     ]
     generation = compile_fragments(generation_files, read_func=read_generation)
-    generation = generation.resample('1h').ffill()
-    generation.dropna(inplace=True, axis='index')
     return generation
 
 
@@ -77,8 +75,12 @@ def compile_temperature():
         for year in [2020, 2021, 2022, 2023, 2024]
     ]
     temperature = compile_fragments(temperature_files, read_func=read_temperature)
-    temperature = temperature.drop(columns=['Zone', 'Kind']).groupby(['Time']).mean()
-    temperature = temperature.resample('1d').ffill()
+    temperature = temperature.reset_index().set_index(['Time', 'Zone'])
+    temperature = temperature.pivot(columns='Kind', values='Degrees F')
+    temperature = temperature.mean(axis='columns').to_frame(name='Degrees F')
+    temperature = temperature.reset_index().set_index('Time')
+    temperature = temperature.pivot(columns='Zone', values='Degrees F')
+
     return temperature
 
 
@@ -90,52 +92,67 @@ def compile_load():
 
     load = compile_fragments(load_files, read_func=read_load)
     load = pd.pivot(load, columns='Zone', values='MW')
-    load = load.resample('1h').ffill()
-    load = load.sum(axis='columns', skipna=False).to_frame(name='MW')
-    load.dropna(inplace=True, axis='index')
     return load
 
 
-def build_dataset(load, lagged_load, temperature):
-    temperature = temperature.reindex(load.index)
-    lagged_load = lagged_load.reindex(load.index)
-    dataset = pd.concat([load, lagged_load, temperature], axis='columns')
-    dataset['Day'] = dataset.index.day
-    dataset['DoW'] = dataset.index.dayofweek
-    dataset['Month'] = dataset.index.month
-    dataset['Hour'] = dataset.index.hour
-    dataset.dropna(inplace=True, axis='index')
+def read_load_forecast(filename):
+    dataset = pd.read_csv(
+        filename,
+        header=1,
+        usecols=[0, 2, 4, 5],
+        names=['Forecast Time', 'Time', 'Zone', 'MW']
+    )
+    dataset['Forecast Time'] = to_datetime(dataset['Forecast Time'])
+    dataset['Time'] = to_datetime(dataset['Time'])
     return dataset
+
+
+def compile_load_forecast():
+    files = [
+        f'raw/load_frcstd_hist-{year}.csv'
+        for year in [2020, 2021, 2022, 2023, 2024]
+    ]
+
+    forecast = compile_fragments(files, read_load_forecast)
+    forecast = pd.pivot(forecast, index=['Time', 'Forecast Time'], columns='Zone', values='MW')
+
+    # Keep only the first forecast (more than 36 hours ahead)
+    forecast = forecast.reset_index().sort_values(['Time', 'Forecast Time']).groupby('Time').first()
+    forecast.drop(columns='Forecast Time', inplace=True)
+
+    return forecast
 
 
 def compile_datasets():
     float_format = '%.0f'
-    generation = compile_generation()
 
-    load = compile_load()
-    lagged_load = load.shift([1, 2, 3], freq='h')
+    load = compile_load().sum(axis='columns', skipna=False).rename('Load MW')
+    forecast = compile_load_forecast()['RTO'].rename('Forecast MW')
+    forecast_error = (load - forecast).to_frame(name='Error MW')
 
-    peak_times = pd.Index(load.resample('1d')['MW'].idxmax(), name='Peak Time')
-    peak_load = load.loc[peak_times]
-    lagged_peak = lagged_load.reindex(peak_times)
+    temperature = compile_temperature().ffill().mean(axis='columns').to_frame('Degrees F')
+    temperature = temperature.reindex(forecast_error.index).ffill()
 
-    temperature = compile_temperature()
-    temperature = temperature.resample('1h').ffill()
+    dataset = pd.concat([forecast_error, temperature], axis='columns')
+    dataset['Day'] = dataset.index.day
+    dataset['DoW'] = dataset.index.dayofweek
+    dataset['Month'] = dataset.index.month
+    dataset['Hour'] = dataset.index.hour
+    dataset = dataset.resample('1h').ffill().dropna(axis='index')
 
-    peak_dataset = build_dataset(load=peak_load, lagged_load=lagged_peak, temperature=temperature)
-    testing = build_dataset(load=load, lagged_load=lagged_load, temperature=temperature)
+    peak_time = pd.Index(dataset.resample('1d')['Error MW'].idxmax(), name='Peak Time')
+    peak_dataset = dataset.loc[peak_time]
 
-    training = peak_dataset[peak_dataset.index < '2024']
-    validating = peak_dataset[peak_dataset.index >= '2024']
-    testing = testing[testing.index >= '2024']
-    correlation_study = load[load.index < '2024']
+    testing = dataset[dataset.index >= '2024']
+    peak_training = peak_dataset[peak_dataset.index < '2024']
+    peak_testing = peak_dataset[peak_dataset.index >= '2024']
 
-    training.to_csv('training.csv', float_format=float_format)
-    validating.to_csv('validating.csv', float_format=float_format)
     testing.to_csv('testing.csv', float_format=float_format)
+    peak_training.to_csv('training.csv', float_format=float_format)
+    peak_testing.to_csv('validation.csv', float_format=float_format)
 
+    generation = compile_generation()
     generation.to_csv('generation.csv', float_format=float_format)
-    correlation_study.to_csv('correlation_study.csv', float_format=float_format)
 
 
 compile_datasets()
