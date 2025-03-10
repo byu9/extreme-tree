c8c1d90db1a253e6ef2ff59a33ababe136429757
John Yu <byu9@ncsu.edu>
2025-03-09 20:34:04 -0400

    use log score as criteria and compute relative reduction to parent

--- extreme_tree/genextreme.py
+++ extreme_tree/genextreme.py
@@ -102,8 +102,8 @@ class GenExtreme:
 
     @staticmethod
     def score_func(parent, left, right):
-        parent_score = _crps_score(parent, GenExtreme.estimate(parent))
-        left_score = _crps_score(left, GenExtreme.estimate(left))
-        right_score = _crps_score(right, GenExtreme.estimate(right))
+        parent_score = _log_score(parent, GenExtreme.estimate(parent))
+        left_score = _log_score(left, GenExtreme.estimate(left))
+        right_score = _log_score(right, GenExtreme.estimate(right))
 
-        return (parent_score - left_score - right_score) / len(parent)
+        return (parent_score - left_score - right_score) / parent_score
