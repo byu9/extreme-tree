864bd7cf0a83254f8444019732769759bed0f918
John Yu <byu9@ncsu.edu>
2025-03-14 15:33:44 -0400

    modify pjm.py
    improve fitting parameters
    add additional columns when saving prediction

--- pjm.py
+++ pjm.py
@@ -26,23 +26,19 @@ def read_prediction(filename):
     return prediction
 
 
-def read_prediction_extended(filename):
-    prediction = read_prediction(filename)
-    predict_dist = genextreme(loc=prediction['mu'], scale=prediction['sigma'], c=-prediction['xi'])
+def write_prediction(filename, model, feature):
+    mu, sigma, xi = model.predict(feature)
+    predict_dist = genextreme(loc=mu.ravel(), scale=sigma.ravel(), c=-xi.ravel())
 
-    extended_data = {
+    prediction_dict = {
+        'mu': mu.ravel(),
+        'sigma': sigma.ravel(),
+        'xi': xi.ravel(),
         'mean': predict_dist.mean().ravel(),
         'lo': predict_dist.ppf(0.05).ravel(),
         'hi': predict_dist.ppf(0.95).ravel(),
         'VaR': predict_dist.isf(11.415e-6).ravel(),
     }
-    extended = pd.DataFrame(extended_data, index=prediction.index)
-    return extended
-
-
-def write_prediction(filename, model, feature):
-    mu, sigma, xi = model.predict(feature)
-    prediction_dict = {'mu': mu.ravel(), 'sigma': sigma.ravel(), 'xi': xi.ravel()}
     prediction = pd.DataFrame(prediction_dict, index=feature.index)
     prediction.to_csv(filename)
 
@@ -52,7 +48,7 @@ def main():
     test_feature, test_target = read_peak_dataset('datasets/pjm/testing.csv')
     validation_feature, validation_target = read_peak_dataset('datasets/pjm/validation.csv')
 
-    model = ExtremeForest(ensemble_size=50, min_score=0.0001, min_partition_size=200)
+    model = ExtremeForest(ensemble_size=50, min_score=0.05, min_partition_size=30)
     model.fit(train_feature, train_target)
 
     write_prediction('training_prediction.csv', model, train_feature)
