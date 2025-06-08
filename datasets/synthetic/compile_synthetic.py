299087720a23ae127e830e5d708f2082b1e3ff97
John Yu <byu9@ncsu.edu>
2025-06-07 23:48:25 -0700

    update compile_synthetic.py
    * seed numpy to get deterministic results
    * fix issues with parameter CSV. Some values are mistakenly written with strings.
    * write true quantiles to file

--- datasets/synthetic/compile_synthetic.py
+++ datasets/synthetic/compile_synthetic.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import genextreme
 
+np.random.seed(123)
+
 
 def compile_datasets():
     x = np.linspace(0, np.pi, 1000)
@@ -11,21 +13,17 @@ def compile_datasets():
     xi = 0.3 * np.cos(5.67 * x)
     sigma = 1 + 0.1 * np.cos(6.78 * x)
 
-    parameters = pd.DataFrame({
-        'x': x,
-        'mu': mu,
-        'sigma': 'sigma',
-        'xi': 'xi',
-    }, index=x)
+    parameters = pd.DataFrame({'x': x, 'mu': mu, 'sigma': sigma, 'xi': xi})
     parameters.to_csv('true_parameters.csv', index_label='index')
 
     dist = genextreme(loc=mu, scale=sigma, c=-xi)
-
-    observations = pd.DataFrame({
-        'x': x,
-        'y': dist.rvs()
-    })
+    observations = pd.DataFrame({'x': x, 'y': dist.rvs()})
     observations.to_csv('observations.csv', index_label='index')
+
+    quantiles = np.array([0.1, 0.5, 0.9, 0.99, 0.999, 0.999999])
+    quantile_values = dist.ppf(quantiles.reshape(-1, 1)).transpose()
+    true_quantiles = pd.DataFrame(quantile_values, columns=quantiles)
+    true_quantiles.to_csv('true_quantiles.csv', index_label='index')
 
 
 compile_datasets()
