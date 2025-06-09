1474372b4f32559ebff555a0ade536115f472c63
John Yu <byu9@ncsu.edu>
2025-06-08 21:30:11 -0700

    rename pjm.py to 110-run_ours_on_pjm.py

--- 110-run_ours_on_pjm.py
+++ 110-run_ours_on_pjm.py
@@ -38,53 +38,37 @@ def read_peak_dataset(filename):
     return feature, target
 
 
-def read_generation():
-    dataset = pd.read_csv('datasets/pjm/generation.csv', index_col=0)
-    dataset.index = pd.to_datetime(dataset.index, utc=True).tz_convert('US/Eastern')
-    dataset = dataset['MW']
-    return dataset
-
-
-def read_prediction(filename):
-    prediction = pd.read_csv(filename, index_col=0)
-    prediction.index = pd.to_datetime(prediction.index, utc=True).tz_convert('US/Eastern')
-    return prediction
-
-
 def write_prediction(filename, model, feature):
     mu, sigma, xi = model.predict(feature)
     predict_dist = genextreme(loc=mu.ravel(), scale=sigma.ravel(), c=-xi.ravel())
+
     eta = 0.1 / 365
     alpha = 1 - eta
 
-    val_at_risk = predict_dist.isf(eta)
-    cond_val_at_risk = conditional_value_at_risk(mu.ravel(), sigma.ravel(), xi.ravel(), alpha=alpha)
+    var = predict_dist.isf(eta)
+    cvar = conditional_value_at_risk(mu.ravel(), sigma.ravel(), xi.ravel(), alpha=alpha)
 
     prediction_dict = {
         'mu': mu.ravel(),
         'sigma': sigma.ravel(),
         'xi': xi.ravel(),
         'mean': predict_dist.mean(),
-        'lo': predict_dist.ppf(0.05),
-        'hi': predict_dist.ppf(0.95),
-        'VaR': val_at_risk,
-        'EUE': (cond_val_at_risk - val_at_risk) * eta
+        'VaR': var,
+        'EUE': (cvar - var) * eta
     }
     prediction = pd.DataFrame(prediction_dict, index=feature.index)
     prediction.to_csv(filename)
 
 
 def main():
-    train_feature, train_target = read_peak_dataset('datasets/pjm/training.csv')
-    test_feature, test_target = read_peak_dataset('datasets/pjm/testing.csv')
-    validation_feature, validation_target = read_peak_dataset('datasets/pjm/validation.csv')
+    train_feature, train_target = read_peak_dataset('datasets/pjm/peak_training.csv')
+    test_feature, test_target = read_peak_dataset('datasets/pjm/peak_testing.csv')
 
     model = ExtremeForest(ensemble_size=50, min_score=0.05, min_partition_size=30)
     model.fit(train_feature, train_target)
 
-    write_prediction('training_prediction.csv', model, train_feature)
-    write_prediction('testing_prediction.csv', model, test_feature)
-    write_prediction('validation_prediction.csv', model, validation_feature)
+    write_prediction('190-run_ours_on_pjm_training.csv', model, train_feature)
+    write_prediction('190-run_ours_on_pjm_testing.csv', model, train_feature)
 
 
 if __name__ == '__main__':
