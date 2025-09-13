13c5948beadc01ea3775c6a3931277118086d33c
John Yu <byu9@ncsu.edu>
2025-09-12 20:39:49 -0400

    add GAMLSS working case

--- 112-run_competitor2_on_pjm.R
+++ 112-run_competitor2_on_pjm.R
@@ -5,17 +5,29 @@ library(gamlssx)
 train <- read.csv('datasets/pjm/peak_training.csv')
 test <- read.csv('datasets/pjm/whole_testing.csv')
 
-fit <- fitGEV(
-    Load.MW ~ pb(Forecast.MW) + pb(Degrees.F) + Day + DoW + Month + Hour,
-    sigma.fo = ~ pb(Forecast.MW) + pb(Degrees.F) + Day + DoW + Month + Hour,
-    nu.fo = ~ pb(Forecast.MW) + pb(Degrees.F) + Day + DoW + Month + Hour,
-    data=train
+# Drop the first colummn (Timestamp)
+train_no_timestamps <- subset(train, select = -1)
+test_no_timestamps <- subset(test, select = -1)
+
+model <- fitGEV(
+    Load.MW ~ Day + DoW + Month + Hour,
+    sigma.fo = ~ Day + DoW + Month + Hour,
+    nu.fo = ~ Day + DoW + Month + Hour,
+    data=train_no_timestamps
 )
 
-mu = fitted(fit, what='mu')
-sigma = fitted(fit, what='sigma')
-xi = fitted(fit, what='nu')
+mu_hat    <- predict(model, what = "mu",  newdata = test_no_timestamps)
+sigma_hat <- predict(model, what = "sigma", newdata = test_no_timestamps)
+xi_hat    <- predict(model, what = "nu", newdata = test_no_timestamps)
 
-print(mu)
-print(sigma)
-print(xi)
+parameter_estimates <- data.frame(index=test$Time,
+    mu_hat=mu_hat,
+    sigma_hat=sigma_hat,
+    xi_hat=xi_hat)
+
+write.csv(
+    parameter_estimates,
+    file='192-run_competitor2_on_pjm.csv',
+    quote=FALSE,
+    row.names=FALSE
+)
